9d61785b10d74a3ef78dab8ff42612aafa797f4d
Yuki Yanai <yanai.yuki@kochi-tech.ac.jp>
2022-05-14 12:08:41 +0900

    add ETAD.R

---
+++ R/ETAD.R
@@ -0,0 +1,11 @@
+#' ETAD: A package for Election Timing in Autocracy and Democracy
+#'
+#' The [ETAD](https://yukiyaai.github.io/ETAD/) packages provides R users with
+#'   the data set of Election Timing in Autocracy and Democracy and useful
+#'   functions to work work with the data.
+#'
+#' @author Masaaki Higashijima, Naoki Shimizu, Hidekuni Washida, and
+#'   Yuki Yanai <yanai.yuki@@kochi-tech.ac.jp>
+#' @docType package
+#' @name ETAD
+NULL

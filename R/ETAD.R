e2692f6ce4ab2b3ecac2586b0790948d8696e1bb
Yuki Yanai <yanai.yuki@kochi-tech.ac.jp>
2022-05-14 19:24:39 +0900

    typo

--- R/ETAD.R
+++ R/ETAD.R
@@ -1,7 +1,7 @@
-#' ETAD: A package for Election Timing in Autocracy and Democracy
+#' ETAD: A package for Election Timing across Autocracy and Democracy
 #'
 #' The [ETAD](https://yukiyaai.github.io/ETAD/) packages provides R users with
-#'   the data set of Election Timing in Autocracy and Democracy and useful
+#'   the data set of Election Timing across Autocracy and Democracy and useful
 #'   functions to work work with the data.
 #'
 #' @author Masaaki Higashijima, Naoki Shimizu, Hidekuni Washida, and
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# ETAD: Election Timing in Autocracy and Democracy
+# ETAD: Election Timing across Autocracy and Democracy
 
 ## Overview
 

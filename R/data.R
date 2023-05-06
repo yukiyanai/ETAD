2c25c3ce9fe56c4ccb79159e6440cf9455e18099
Yuki Yanai <yanai.yuki@kochi-tech.ac.jp>
2023-05-06 18:33:48 +0900

    upload ver 0.0.1

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' ETAD data
+#'
+#' Data of Election Timing across Autocracy and Democracy
+#'
+#' @format ## `ETAD`
+#' A data frame with 3,099 rows and 19 columns:
+#' \describe{
+#'   \item{cowcode}{COW (Correlates of War) country code}
+#'   \item{iso3}{3 letter ISO country code}
+#'   \item{country}{Country name}
+#'   \item{etype2}{Binary variable indicating the election type: "presidenial" or "legislative"}
+#'   \item{etype3}{Trichotomous variable indicating the election type: "presidential", "lower house", or "upper house. The current version does not contain "upper house" elections.}
+#'   \item{parliamentarism}{Dummy indicating the parliamentary system. This is a "factor" variable where `0` is labeled as "presidentialism" and `1` as "parliamentarism". }
+#'   \item{timing1}{Trichotomous variable showing election timing. `1` = held as scheduled, `2` = early or delayed election, and `3` = cannot tell if an election was held as scheduled or not.}
+#'   \item{timing2}{Trichotomous variable showing election timing. `-1` = an early election, `0` = on-schedule election, and `1` = delayed election. `NA` if `timing1` == 3.}
+#'   \item{reason}{Categorical variable showing the resons for the election-timing change. `0` = no change, `1` = the first election under the new constitution or for the new state, `2` = re-election due to an invalid election, `3` = coup, `4` = natural death of the leader, `5` = the leader was assassinated, `6` = civil war, `9` = other.}
+#'   \item{held_date}{Date when the election was held.}
+#'   \item{held_year}{Year when the election was held.}
+#'   \item{held_month}{Month when the election was held.}
+#'   \item{held_wday}{Day of the week when the election was held.}
+#'   \item{sche_date}{Date when the election was originally scheduled.}
+#'   \item{sche_year}{Year when the election was originally scheduled.}
+#'   \item{sche_month}{Month when the election was originally scheduled.}
+#'   \item{sche_wday}{Day of the week when the election was originally scheduled.}
+#'   \item{dif_days}{Difference between `held_date` and `sche_date`. A negative value means the election was held earlier than scheduled, and a positive means it was delayed.}
+#'   \item{termlength}{Term length in year.}
+#' }
+"ETAD"
---
+++ data-raw/ETAD.R
@@ -0,0 +1,82 @@
+## code to prepare `ETAD` dataset
+##
+## 2023-05-06 Yuki Yanai
+##
+
+library(tidyverse)
+library(countrycode)
+
+D <- read_csv("data-raw/unprepared/etad_to2022_20230506.csv")
+
+D2 <- D |>
+  select(ccode:need2fix) |>
+  rename(cowcode = ccode) |>
+  mutate(iso3 = countrycode(cowcode,
+                            origin = "cown",
+                            destination = "iso3c")) |>
+  mutate(etype2 = ifelse(etype2 == "president",
+                         "presidential",
+                         "legislative")) |>
+  mutate(etype3 = case_when(
+    etype3 == "LH" ~ "lower house",
+    etype3 == "UH" ~ "upper house",
+    etype3 == "President" ~ "presidential")) |>
+  rename(parliamentarism = binary_system_cp) |>
+  mutate(parliamentarism = ifelse(parliamentarism == "presidentialism", 0L, 1L)) |>
+  mutate(parliamentarism = factor(parliamentarism,
+                                  levels = 0 : 1,
+                                  labels = c("presidentialism",
+                                             "parliamentarism"))) |>
+  rename(timing1 = timing,
+         timing2 = timing_tri) |>
+  mutate(sche_year = year(sche_date),
+         sche_month = month(sche_date),
+         sche_wday = wday(sche_date, label = TRUE, abbr = FALSE)) |>
+  mutate(held_year = year(held_date),
+         held_month = month(held_date),
+         held_wday = wday(held_date, label = TRUE, abbr = FALSE)) |>
+  mutate(sche_date = as.Date(sche_date, format = "%Y-%m-%d"),
+         held_date = as.Date(held_date, format = "%Y-%m-%d")) |>
+  mutate(dif_days = as.integer(held_date - sche_date)) |>
+  mutate(new_reason = case_when(
+    reason == 0 ~ 0L,
+    reason %in% c(6, 8) ~ 1L,
+    reason == 7 ~ 2L,
+    reason == 1 ~ 3L,
+    reason == 3 ~ 4L,
+    reason == 2 ~ 5L,
+    reason == 4 ~ 6L,
+    reason == 5 ~ 9L)) |>
+  select(!reason) |>
+  rename(reason = new_reason)
+
+# reasonの元コード
+#1. クーデタ
+#2. 指導者の暗殺
+#3. 指導者の自然死
+#4. 内戦
+#5. その他
+#6. 新国家樹立
+#7. 選挙の無効に伴う再選挙
+#8. 新憲法下の第1回選挙（6に統合してもいいかもしれません）
+
+# reason の変換後のコード
+#0.選挙タイミングの変更なし
+#1. 新憲法制定 or 新国家樹立
+#2. 選挙の無効に伴う再選挙
+#3. クーデタ
+#4. 指導者の自然死
+#5. 指導者の暗殺
+#6. 内戦
+#9. その他
+
+ETAD <- D2 |>
+  select(cowcode, iso3, country,
+         etype2, etype3, parliamentarism,
+         timing1, timing2, reason,
+         held_date, held_year, held_month, held_wday,
+         sche_date, sche_year, sche_month, sche_wday,
+         dif_days, termlength)
+
+# Run the following to update the dataset.
+usethis::use_data(ETAD, overwrite = TRUE)
